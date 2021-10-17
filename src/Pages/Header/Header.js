baf748c32764894c2e3c6baeb155c5c82463a4dc
Md Auwal Hossain <mdauwalhossain2020@gmail.com>
2021-10-18 04:23:57 +0600

    Carousoul ok

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Carousel, Container, Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import './Header.css'
 
@@ -17,6 +17,44 @@ const Header = () => {
                 </Nav>
                 </Container>
             </Navbar>
+
+
+            <Carousel>
+                <Carousel.Item interval={1000}>
+                    <img
+                    className="d-block w-100"
+                    src="https://cdn-caimh.nitrocdn.com/yYKLjJHXYJzwvfGYwbksgHLtEXQTAPwb/assets/static/optimized/wp-content/uploads/2019/05/8f440c4ce0b0eadf3f5dba838c9e10e9.Order-4411-_-Image-7-750x400.jpg"
+                    alt="First slide"
+                    />
+                    <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={500}>
+                    <img
+                    className="d-block w-100"
+                    src="https://images.squarespace-cdn.com/content/v1/5a3a8587ccc5c5f14bdd3b7c/1514502889485-3LW3OEYGHE2O766QLDQ0/3+reasons.jpg?format=1500w"
+                    alt="Second slide"
+                    />
+                    <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="https://www.medoradental.com/wp-content/uploads/2018/08/MedoraDental-650737298.jpg"
+                    alt="Third slide"
+                    />
+                    <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+
         </div>
     );
 };
